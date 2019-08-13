02620790b9ab5ef6ed2f9b67dcd150e2be39e0fe
Duy Tuấn <duytuan2002@gmail.com>
2019-08-13 10:49:35 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,223 @@
+<html>
+<head>
+	<meta charset='utf-8'>
+	<meta content='Trang tổng hợp tìm kiếm và liên kết quan trọng' name='description'/>
+	<meta content='Trang tổng hợp tìm kiếm và liên kết quan trọng' property='og:description'/>
+	<meta content='Tìm kiếm mỗi ngày' property='og:title'/>
+	<title>Tìm kiếm mỗi ngày</title>
+<style>
+	#wrap {
+		width: 70%;		
+		padding: 20px;
+		border-radius: 10px;
+		border: 1px solid #ddd;
+		font-size: 20px;
+		line-height: 1.3em;
+		text-align: center;
+		align:center;
+	}
+	#thanhtren {
+		text-align: center;
+		align:center;
+	}
+	#ThanhTren img{
+		width:64px;
+		height: 64px;
+		padding: 5px;
+		border: 2px solid white;
+	}
+	#thanhtren img:hover {
+		
+		border: 2px solid blue;
+	}
+	#trangduoi a{	
+		font-size: 16px;
+	}
+	#trangduoi li{
+		padding-top: 5px;		
+	}
+	#tieude{
+		font-size: 18px;
+		font-weight: bold;		
+	}
+	#trangduoi {
+		font-size: 14px;
+		padding: 20px;
+		border: 1px solid #ddd;
+		border-radius: 5px;
+		width: 70%;	
+	}
+	#trangduoi img{		
+		width:14px;
+		height: 14px;
+	}
+	#benphai .demuc img{		
+		width:14px;
+		height: 14px;
+	}
+	#benphai {
+		width: 26%;
+		float: right;
+		border: 1px solid #ddd;
+		border-radius: 5px;
+	}
+</style>
+
+<!-- Global site tag (gtag.js) - Google Analytics ---- Xong roi thi chuyen len -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-11448994-11"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-11448994-11');
+</script>
+
+
+</head>
+<body>
+		
+		<div id="thanhtren">		
+			<a href="https://www.youtube.com/" target="_blank" title="Trang tổng hợp Video, clip"><img src="https://www.google.com/s2/favicons?sz=64&domain=https://www.youtube.com/" alt="icon Youtube" /></a>
+			<a href="https://mail.google.com/" target="_blank" title="Google Mail"><img src="https://ssl.gstatic.com/ui/v1/icons/mail/rfr/logo_gmail_lockup_default_1x.png" alt="icon GM" /></a>			
+			<a href="https://photos.google.com/" target="_blank" title="Google Photo"><img src="https://www.google.com/s2/favicons?sz=64&domain=https://photos.google.com/" alt="icon GP" /></a>			
+			<a href="https://www.tuoitre.vn/" target="_blank" title="Tuổi trẻ"><img src="https://www.google.com/s2/favicons?sz=64&domain=https://www.tuoitre.vn/" alt="icon TT"/></a>
+			<a href="https://vn3000.com/" target="_blank" title="VOA Tiếng Việt"><img src="https://www.google.com/s2/favicons?sz=64&domain=https://www.voatiengviet.com" alt="icon VOA"/></a>
+			<a href="https://www.rfa.org/vietnamese/" target="_blank" title="Đài Á Châu Tự Do"><img src="https://www.google.com/s2/favicons?sz=64&domain=https://www.rfa.org/vietnamese/" alt="icon RFA"/></a>
+			<a href="https://www.facebook.com/" target="_blank" title="Facebook"><img src="https://www.google.com/s2/favicons?sz=64&domain=https://www.facebook.com/" alt="icon FB"/></a>
+		
+		</div>		
+		<div id="benphai">
+			Quảng cáo
+			<!-- postbit -- xong roi thi chuyen len -->
+			<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+		
+			<ins class="adsbygoogle"
+			 style="display:block"
+			 data-ad-client="ca-pub-6104854674744406"
+			 data-ad-slot="9182238418"
+			 data-ad-format="auto"
+			 data-full-width-responsive="true"></ins>
+			<script>
+			 (adsbygoogle = window.adsbygoogle || []).push({});
+			</script>
+			 
+			
+		<!-- Random post -->
+		<br/>
+			Chuyện cười hôm nay
+			<ul id='random-posts'>
+			<script type='text/javaScript'>
+			var rdp_numposts=5;
+			var rdp_snippet_length=100;
+			var rdp_info='no';
+			var rdp_comment='Nhận xét';
+			var rdp_disable='Tắt Nhận xét';
+			var rdp_current=[];var rdp_total_posts=0;var rdp_current=new Array(rdp_numposts);function totalposts(json){rdp_total_posts=json.feed.openSearch$totalResults.$t}document.write('<script type=\"text/javascript\" src=\"https://tieulam.thiamlau.com/feeds/posts/default?alt=json-in-script&max-results=0&callback=totalposts\"><\/script>');
+			function getvalue(){for(var i=0;i<rdp_numposts;i++){var found=false;var rndValue=get_random();for(var j=0;j<rdp_current.length;j++){if(rdp_current[j]==rndValue){found=true;break}};if(found){i--}else{rdp_current[i]=rndValue}}};
+			function get_random(){var ranNum=1+Math.round(Math.random()*(rdp_total_posts-1));return ranNum};
+			</script>
+			<script type='text/javaScript'>
+			function random_posts(json){a=location.href;y=a.indexOf('?m=0');for(var i=0;i<rdp_numposts;i++){var entry=json.feed.entry[i];var rdp_posttitle=entry.title.$t;if('content'in entry){var rdp_get_snippet=entry.content.$t}else{if('summary'in entry){var rdp_get_snippet=entry.summary.$t}else{var rdp_get_snippet="";}};rdp_get_snippet=rdp_get_snippet.replace(/<[^>]*>/g,"");if(rdp_get_snippet.length<rdp_snippet_length){var rdp_snippet=rdp_get_snippet}else{rdp_get_snippet=rdp_get_snippet.substring(0,rdp_snippet_length);var space=rdp_get_snippet.lastIndexOf(" ");rdp_snippet=rdp_get_snippet.substring(0,space)+"&#133;";};for(var j=0;j<entry.link.length;j++){if('thr$total'in entry){var rdp_commentsNum=entry.thr$total.$t+' '+rdp_comment}else{rdp_commentsNum=rdp_disable};if(entry.link[j].rel=='alternate'){var rdp_posturl=entry.link[j].href;if(y!=-1){rdp_posturl=rdp_posturl+'?m=0'}var rdp_postdate=entry.published.$t;}};
+			document.write('<li>');document.write('<a href="'+rdp_posturl+'" title="'+rdp_snippet+'">');
+			document.write(rdp_posttitle+'</a>');if(rdp_info=='yes'){document.write('<span>'+rdp_postdate.substring(8,10)+'/'+rdp_postdate.substring(5,7)+'/'+rdp_postdate.substring(0,4)+' - '+rdp_commentsNum)+'</span>'}document.write('<div style="clear:both"></div></li>')}};
+			getvalue();for(var i=0;i<rdp_numposts;i++){document.write('<script type=\"text/javascript\" src=\"https://tieulam.thiamlau.com/feeds/posts/default?alt=json-in-script&start-index='+rdp_current[i]+'&max-results=1&callback=random_posts\"><\/script>')};
+			</script></ul>
+			<div class="demuc">
+			<div id="tieude">Giáo dục</div> <br/>
+				<li><a href="https://thuthuataccess.com/forum/" target="_blank" rel="follow, index" title="Diễn đàn chia sẻ và hướng dẫn làm việc công cụ MS Access"><img src="https://www.google.com/s2/favicons?sz=32&domain=https://thuthuataccess.com/" alt="icon TTAC" > Thủ Thuật Access</a>
+				: Diễn đàn chia sẻ và hướng dẫn làm việc công cụ MS Access</li>
+				<li><a href="https://nhatkyit.thuthuataccess.com/" target="_blank" rel="follow, index" title="Blog lưu lại nhưng vấn đề gặp phải trong quá trình làm việc"><img src="https://www.google.com/s2/favicons?sz=32&domain=https://nhatkyit.thuthuataccess.com/" alt="icon IT" > Nhật ký IT</a>
+				: Blog lưu lại nhưng vấn đề gặp phải trong quá trình làm việc<br/></li>
+				<li><a href="https://excel.thuthuataccess.com/" target="_blank" rel="follow, index" title="Blog lưu lại bài học, thủ thuật, giải pháp làm việc với Excel"><img src="https://www.google.com/s2/favicons?sz=32&domain=https://excel.thuthuataccess.com/" alt="icon GPEX" > Tự học Excel</a>
+				: Blog lưu lại bài học, thủ thuật, giải pháp làm việc với Excel<br/></li>
+				=============================================<br/>
+				<div id="tieude">Giải trí</div> <br/>
+				<li><a href="https://www.youtube.com/" target="_blank" rel="follow, index" title="Trang tổng hợp Video, clip"><img src="https://www.google.com/s2/favicons?sz=32&domain=https://www.youtube.com/" alt="icon Youtube" > Youtube</a>
+				: Trang tổng hợp Video, clip </br></li>
+				<li><a href="http://thiamlau.com/forum/" target="_blank" rel="follow, index" title="Diễn đàn thơ văn nhạc truyện"><img src="https://www.google.com/s2/favicons?sz=32&domain=https://thiamlau.com/" alt="icon TAL" > Diễn đàn Thi Ẩm Lâu</a>
+				: Diễn đàn thơ văn nhạc truyện</br></li>
+				<li><a href="http://cunghoangdao.thuthuataccess.com/" target="_blank" rel="follow, index" title="Blog xem bói vui về 12 cung hoàng đạo"><img src="https://www.google.com/s2/favicons?sz=32&domain=http://cunghoangdao.thuthuataccess.com/" alt="icon CHD" > 12 Cung Hoàng Đạo</a>	
+				: Blog xem bói vui về 12 cung hoàng đạo </br>
+				<li><a href="http://truyen.thiamlau.com/" target="_blank" rel="follow, index" title="Kho truyện sưu tầm"><img src="https://www.google.com/s2/favicons?sz=32&domain=http://truyen.thiamlau.com/" alt="icon Truyen" > Đọc truyện</a>
+				: Kho truyện sưu tầm<br></li>
+				<li><a href="http://loinhac.thiamlau.com/" target="_blank" rel="follow, index" title="Kho lời nhạc sưu tầm"><img src="https://www.google.com/s2/favicons?sz=32&domain=http://loinhac.thiamlau.com/" alt="icon loinhac" > Tìm lời bài hát</a>
+				: Kho lời nhạc sưu tầm<br></li>
+				<li><a href="https://www.facebook.com/" target="_blank" title="Facebook"><img src="https://www.google.com/s2/favicons?sz=32&domain=https://www.facebook.com/" alt="icon FB"> Facebook</a>
+				: Mạng xã hội lớn nhất hành tinh<br></li> 
+			</div>
+			<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+			<script>
+				 (adsbygoogle = window.adsbygoogle || []).push({
+					  google_ad_client: "ca-pub-6104854674744406",
+					  enable_page_level_ads: true
+				 });
+			</script>
+		</div>
+		<div id="wrap">
+			
+			Tìm kiếm trên Google
+			<!-- Mã hộp tìm kiếm 
+			
+			
+			-->
+			<script>
+			  (function() {
+				var cx = 'partner-pub-6104854674744406:6543159872';
+				var gcse = document.createElement('script');
+				gcse.type = 'text/javascript';
+				gcse.async = true;
+				gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+				var s = document.getElementsByTagName('script')[0];
+				s.parentNode.insertBefore(gcse, s);
+			  })();
+			</script>
+			<gcse:searchbox-only></gcse:searchbox-only>
+		<!-- Mã truy vấn phổ biến -->
+		<script type="text/javascript" src="https://www.google.com/cse/query_renderer.js"></script>
+			<div id="queries"></div>
+			<script src="https://www.google.com/cse/api/partner-pub-6104854674744406/cse/6609138575/queries/js?oe=UTF-8&amp;callback=(new+PopularQueryRenderer(document.getElementById(%22queries%22))).render"></script>
+
+			<!-- Mã truy vấn phổ biến -->
+			<script type="text/javascript" src="https://www.google.com/cse/query_renderer.js"></script>
+			<div id="queries"></div>
+			<script src="https://www.google.com/cse/api/partner-pub-6104854674744406/cse/6543159872/queries/js?oe=UTF-8&amp;callback=(new+PopularQueryRenderer(document.getElementById(%22queries%22))).render"></script>
+			
+			<!-- end -->
+		</div>
+			<div id="trangduoi">
+				=========================================</br>
+				<div id="tieude">Tin Tức</div></br>
+				<li><a href="https://www.tuoitre.vn/" target="_blank" title="Tuổi trẻ"><img src="https://www.google.com/s2/favicons?sz=32&domain=https://www.tuoitre.vn/" alt="icon TT"> Tuổi trẻ</a>
+				: Trang tin tức báo Tuổi trẻ<br></li>
+				<li><a href="https://www.vnexpress.net/" target="_blank" title="VN Express"><img src="https://www.google.com/s2/favicons?sz=32&domain=https://www.vnexpress.net" alt="icon VNE"> VN Express</a>
+				: Trang tin tức báo VN Express<br></li>	
+				<li><a href="search-rfa.php" target="_blank" title="VOA"><img src="https://www.google.com/s2/favicons?sz=32&domain=https://www.rfa.org/vietnamese/" alt="icon RFA"/> RFA</a>
+				: Điểm tin RFA<br></li>					
+				<li>
+					<?php
+						$xml = simplexml_load_file("http://feeds.feedburner.com/ThThutAccess-AllForums");
+							foreach ($xml ->children() as $value){
+								foreach($value->children() as $value1){
+									$s1=$value1->title;
+									$s2=$value1->description;
+									$s3=$value1->pubDate;
+									$s4=$value1->link;
+									if ($s4!=""){
+										echo "<a href='".$s4."'>".$s1."</a><br/>";
+										echo $s3."<br/>";
+										echo $s2."<br/>";
+									}
+								}
+							}							
+					?>
+				</li>
+				==========================================<br/>
+				<div id="tieude">Doanh Nghiệp</div></br>
+				<li><a href="https://www.pmanhatrang.com/" target="_blank" title="PMA Nha Trang"><img src="https://www.google.com/s2/favicons?sz=32&domain=http://www.pmanhatrang.com" alt="icon PMA"> PMA Nha Trang</a>
+				: Công ty sản xuất và gia công và trang trí trang gỗ dán cạnh PMA<br></li>
+				<li><a href="https://www.nhahangsongtho.com/" target="_blank" title="Nhà Hàng Sông Thơ"><img src="https://www.google.com/s2/favicons?sz=32&domain=http://www.nhahangsongtho.com" alt="icon ST"> Nhà Hàng Sông Thơ</a>
+				: Nhà hàng ven sông với không gian thoáng đãng mát mẻ, thơ mộng<br></li>
+			</div>
+	</body>
+</html>
